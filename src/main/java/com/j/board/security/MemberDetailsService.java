403be56bad0ad2f233051ccdc9c68b20c2afa784
rio <mario64aq@gmail.com>
2019-08-16 00:15:10 +0900

    Add springsecurity userdetailsServeice

---
+++ src/main/java/com/j/board/security/MemberDetailsService.java
@@ -0,0 +1,29 @@
+package com.j.board.security;
+
+import com.j.board.domain.MemberVO;
+import com.j.board.persistence.MemberMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberDetailsService implements UserDetailsService {
+  @Autowired
+  private MemberMapper memberMapper;
+
+  @Override
+  public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
+    MemberVO vo = null;
+    try {
+      vo = memberMapper.findByUserid(memberId);
+
+    } catch (UsernameNotFoundException e) {
+      //TODO error handler
+    }
+    return new CustomMember(vo);
+  }
+  
+}
