4af3fe1ceefe7903a9cc731c4b8f24c380535360
rio <mario64aq@gmail.com>
2019-08-16 00:58:50 +0900

    Add signUpMemberTest to meberServiceTest class

---
+++ src/test/java/com/j/board/service/MemberServiceTest.java
@@ -0,0 +1,45 @@
+package com.j.board.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import com.j.board.domain.MemberVO;
+import com.j.board.persistence.MemberMapper;
+import com.j.board.service.MemberServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class MemberServiceTest {
+  @Mock
+  private MemberMapper memberMapper;
+  private MemberVO memberVO;
+
+  @Mock
+  BCryptPasswordEncoder bCryptPasswordEncoder;
+
+  @InjectMocks
+  MemberServiceImpl memberService;
+
+  @Before
+  public void setUp() throws Exception {
+    Timestamp today = new Timestamp(new Date().getTime());
+    memberVO = new MemberVO("test0","testpasswd","testname", today, "ROLE_USER",today);
+  }
+
+  @Test
+  public void signUpMemberTest() {
+    when(memberMapper.signUpMember(memberVO)).thenReturn(true);
+    boolean result = memberService.signUpMember(memberVO);
+
+    assertEquals(true, result);
+  }
+}
