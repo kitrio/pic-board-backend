64f48ea2ba8913184ad0b300fd609cba56415b56
rio <mario64aq@gmail.com>
2019-08-15 19:58:38 +0900

    Add member model

---
+++ src/main/java/com/j/board/domain/MemberVO.java
@@ -0,0 +1,49 @@
+package com.j.board.domain;
+
+import java.sql.Timestamp;
+
+public class MemberVO {
+	private String memberId;
+  private String password;
+  private String nickname;
+  private Timestamp regDate;
+  private String authInfo;
+  private Timestamp lastLogin;
+	  
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public Timestamp getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Timestamp regDate) {
+		this.regDate = regDate;
+	}
+	public String getAuthInfo() {
+		return authInfo;
+	}
+	public void setAuthInfo(String authInfo) {
+		this.authInfo = authInfo;
+	}
+	public Timestamp getLastLogin() {
+		return lastLogin;
+	}
+	public void setLastLogin(Timestamp lastLogin) {
+		this.lastLogin = lastLogin;
+	}
+}
