8370fd0d551dbcefd7a6ae2fd0cad2732495cda6
rio <mario64aq@gmail.com>
2019-08-16 21:41:06 +0900

    Add ErrorPath controller

---
+++ src/main/java/com/j/board/controller/ErrorPathController.java
@@ -0,0 +1,21 @@
+package com.j.board.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErrorPathController implements ErrorController{
+
+    @GetMapping("error")
+    public ResponseEntity<ErrorPathController> responseError(){
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    
+    @Override
+    public String getErrorPath(){
+        return "/error";
+    }
+}
