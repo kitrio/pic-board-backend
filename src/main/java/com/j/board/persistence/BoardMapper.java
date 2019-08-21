22af7d38ce82325931c7efc6507d6edf2d49f49a
kit rio <mario64aq@gmail.com>
2019-08-21 23:21:02 +0900

    Add insertFile to BoardMapper

--- src/main/java/com/j/board/persistence/BoardMapper.java
+++ src/main/java/com/j/board/persistence/BoardMapper.java
@@ -4,11 +4,15 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.j.board.domain.BoardVO;
+import com.j.board.domain.FilesVO;
 
 
 @Mapper
 public interface BoardMapper {
 	@Insert("insert into board(boardNum, writer, title, content, readCount, goodCount, ip) VALUES(#{boardNum}, #{nickname}, #{title}, #{content}, #{readCount}, #{goodCount}, #{ip})")
-    public void insertContent(BoardVO vo);
+    public boolean insertContent(BoardVO board);
+
+    @Insert("insert into files (boardnum, filename, filealtname, filepath) VALUSE(#{boardNum}, #{filename}, #{fileAltName}, #{filePath}")
+    public boolean insertFile(FilesVO files);
 
 }
