715da8b247ada2a6a86df43be42f7a5891ca6548
kit rio <mario64aq@gmail.com>
2019-08-23 21:48:52 +0900

    Add uploadImg to boardController

--- src/main/java/com/j/board/controller/BoardController.java
+++ src/main/java/com/j/board/controller/BoardController.java
@@ -6,21 +6,30 @@
  import org.springframework.web.bind.annotation.PostMapping;
  import org.springframework.web.bind.annotation.RequestBody;
  import org.springframework.web.bind.annotation.RequestMapping;
- import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
- import com.j.board.domain.BoardVO;
- import com.j.board.service.BoardListService;
+import com.j.board.domain.BoardVO;
+import com.j.board.service.BoardListService;
+import com.j.board.service.FileService;
 
  @RestController
  @RequestMapping("list")
  public class BoardController {
  	@Autowired
- 	BoardListService boardListService;
+     BoardListService boardListService;
+     FileService fileService;
      @PostMapping("/write")
      public void writeContent(@RequestBody BoardVO contentVO,HttpServletRequest request) {
      	String ip = getIpAddress(request);
          contentVO.setIp(ip);
          boardListService.writeService(contentVO);
+     }
+
+     @PostMapping("/write/image")
+     public String uploadImg(@RequestPart MultipartFile imgfile) {
+        return fileService.upLoadFile(imgfile);
      }
     
      private String getIpAddress(HttpServletRequest request) {
