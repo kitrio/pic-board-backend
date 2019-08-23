1314c971daf50068daf231f8f3053db141801e68
kit rio <mario64aq@gmail.com>
2019-08-23 21:44:03 +0900

    Add setRealfileNum to BoardMapper

--- src/main/java/com/j/board/persistence/BoardMapper.java
+++ src/main/java/com/j/board/persistence/BoardMapper.java
@@ -2,6 +2,7 @@ package com.j.board.persistence;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
 
 import com.j.board.domain.BoardVO;
 import com.j.board.domain.FilesVO;
@@ -14,5 +15,8 @@ public interface BoardMapper {
 
     @Insert("insert into files (boardnum, filename, filealtname, filepath) VALUSE(#{boardNum}, #{filename}, #{fileAltName}, #{filePath}")
     public boolean insertFile(FilesVO files);
+    
+    @Update("update files boardnum = #{boardNum}, where filealtname = #{fileAltName}")
+	public void setRealfileNum(int boardNum, String fileAltName);
 
 }
