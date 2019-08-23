2d39cf5772c3c6c56ec8fc264cda182b3868ec85
kit rio <mario64aq@gmail.com>
2019-08-23 22:38:27 +0900

    Fix FileUpload path

--- src/main/java/com/j/board/service/FileServiceImpl.java
+++ src/main/java/com/j/board/service/FileServiceImpl.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,17 +15,20 @@ import com.j.board.persistence.BoardMapper;
 public class FileServiceImpl implements FileService{
 	@Autowired
 	BoardMapper boardMapper;
+
+	@Value("${file_save_path}")
+	private String SAVE_PATH;
+	
 	public String upLoadFile(MultipartFile file) {
 		String fileExtention = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
 		String uuid = UUID.randomUUID().toString();
-		String savePath = "./resource/static";
 		FilesVO filesVO = new FilesVO();
 		filesVO.setBoardNum(-1); // Temp file number
-        filesVO.setFileName(file.getName());
+        filesVO.setFileName(file.getOriginalFilename());
         filesVO.setFileAltName(uuid + fileExtention);
-		filesVO.setFilePath(savePath+ filesVO.getFileAltName());
+		filesVO.setFilePath(SAVE_PATH+ filesVO.getFileAltName());
 
-		File saveFile = new File(savePath + filesVO.getFileAltName());
+		File saveFile = new File(SAVE_PATH + filesVO.getFileAltName());
 		try {
 			file.transferTo(saveFile);
 			boardMapper.insertFile(filesVO);
