8af5c9c9b0c3eea668a87c03898b50313471cad3
kit rio <mario64aq@gmail.com>
2019-08-24 12:06:53 +0900

    Remove content's date setting from BoardlistService

--- src/main/java/com/j/board/service/BoardListServiceImpl.java
+++ src/main/java/com/j/board/service/BoardListServiceImpl.java
@@ -1,8 +1,5 @@
 package com.j.board.service;
 
-import java.util.Date;
-import java.sql.Timestamp;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +11,6 @@ public class BoardListServiceImpl implements BoardListService{
 	@Autowired
 	BoardMapper boardMapper;
 	public void writeService(BoardVO contentVO) {
-		Date systemTime = new Date();
-        Timestamp date = new java.sql.Timestamp(systemTime.getTime());
-		contentVO.setWriteTime(date);
 		boardMapper.insertContent(contentVO);
 	}
 }
