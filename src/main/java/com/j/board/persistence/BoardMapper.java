3d3e8e327f76a3afe15d24220656997aa50394d3
kit rio <mario64aq@gmail.com>
2019-08-25 20:21:35 +0900

    Add select contentsList to BoardMapper

--- src/main/java/com/j/board/persistence/BoardMapper.java
+++ src/main/java/com/j/board/persistence/BoardMapper.java
@@ -2,8 +2,12 @@ package com.j.board.persistence;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 import com.j.board.domain.BoardVO;
 import com.j.board.domain.FilesVO;
@@ -18,6 +22,12 @@ public interface BoardMapper {
 
     @Select("select * from board where boardnum = #{boardNum}")
     public BoardVO selectOneContent(int boardNum);
+
+    @Select("select * from board where boardnum = #{boardNum} limit #{firstPage}, #{lastPage}")
+    public List<BoardVO> selectContentsList (@Param("firstPage") int firstPage, @Param("lastPage") int lastPage);
+    
+    @Select("select * from board where writetime between #{startDay} AND #{endDay} order by goodcount desc LIMIT 0, 19")
+    public List<BoardVO> selectWeeklyBestList (@Param("startDay") Timestamp statDay, @Param("endDay") Timestamp endDay);
 
     @Update("update board content = #{content}, title= #{title}, filealtname= #{fileAltName}, where boardnum = #{boardNum} ")
     public boolean updateContent(BoardVO board);
