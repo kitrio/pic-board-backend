1e4b71c52d3d032a9f379d71d0cd24acff2e1a38
kit rio <mario64aq@gmail.com>
2019-08-25 15:51:47 +0900

    Add member nickname in MemberContorller

--- src/main/java/com/j/board/controller/MemberController.java
+++ src/main/java/com/j/board/controller/MemberController.java
@@ -1,11 +1,15 @@
 package com.j.board.controller;
 
+import java.security.Principal;
+
 import com.j.board.domain.MemberVO;
+import com.j.board.security.CustomMember;
 import com.j.board.service.MemberService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +31,15 @@ public class MemberController {
       else{
         return new ResponseEntity<>(HttpStatus.CONFLICT);
       }
+  }
+  @PostMapping("/info")
+  public ResponseEntity<String> getMemberNickname(Principal principal) {
+    CustomMember user = (CustomMember) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if(user != null){
+      return new ResponseEntity<>(user.getMemberVO().getNickname(),HttpStatus.OK);
+    }  else {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
   }
   
 
