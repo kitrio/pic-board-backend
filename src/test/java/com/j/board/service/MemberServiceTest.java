e85069586eaa6259d45892908f4a02bde905c29e
kit rio <mario64aq@gmail.com>
2020-03-28 17:49:25 +0900

    Add test searchMemberContents

--- src/test/java/com/j/board/service/MemberServiceTest.java
+++ src/test/java/com/j/board/service/MemberServiceTest.java
@@ -4,11 +4,15 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import com.j.board.domain.BoardVO;
 import com.j.board.domain.MemberVO;
 import com.j.board.persistence.MemberMapper;
-import com.j.board.service.MemberServiceImpl;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +25,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class MemberServiceTest {
   @Mock
   private MemberMapper memberMapper;
+  @Mock
   private MemberVO memberVO;
 
   @Mock
@@ -29,10 +34,30 @@ public class MemberServiceTest {
   @InjectMocks
   MemberServiceImpl memberService;
 
+  List<BoardVO> boardVOs = new ArrayList<>();
+  public LocalDate localTimeStd;
+
   @Before
   public void setUp() throws Exception {
     Timestamp today = new Timestamp(new Date().getTime());
-    memberVO = new MemberVO("test","testpasswd","testname", today, today,"ROLE_USER");
+    localTimeStd = LocalDate.of(2020, 01, 1);
+    
+    
+    memberVO= new MemberVO("test","testpasswd","testname", today, today,"ROLE_USER");
+    BoardVO postOne = new BoardVO();
+    postOne.setBoardNum(1);
+    postOne.setTitle("test title");
+    postOne.setContent("content");
+    postOne.setMemberId("id");
+    postOne.setNickname("nickname");
+    postOne.setWriteTime(localTimeStd.toString());
+    postOne.setGoodCount(1);
+    postOne.setReadCount(1);
+    postOne.setFileAltName("test.jpg");
+    postOne.setIp("127.0.0.1");
+
+    boardVOs.add(0,postOne);
+    
   }
 
   @Test
@@ -41,5 +66,11 @@ public class MemberServiceTest {
     boolean result = memberService.signUpMember(memberVO);
     
     assertEquals(true, result);
+  }
+
+  @Test
+  public void searchMemberContentsTest() {
+    when(memberMapper.selectByMemberContent("test")).thenReturn(boardVOs);
+    assertEquals(boardVOs.get(0).getBoardNum(), memberService.searchMemberContents("test").get(0).getBoardNum());
   }
 }
