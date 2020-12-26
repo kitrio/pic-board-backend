95036bd2fb326a564127caf82a1a9d9f2a23cafc
kit rio <mario64aq@gmail.com>
2020-12-26 15:52:22 +0900

    Fix selectContentsList

--- src/main/java/com/j/board/persistence/BoardMapper.java
+++ src/main/java/com/j/board/persistence/BoardMapper.java
@@ -1,11 +1,6 @@
 package com.j.board.persistence;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 
 import java.sql.Timestamp;
 import java.util.List;
@@ -15,29 +10,32 @@ import com.j.board.domain.FilesVO;
 
 @Mapper
 public interface BoardMapper {
-    @Insert("insert into board(boardNum, nickname, memberid, title, content, readCount, goodCount, ip, filealtname) VALUES(#{boardNum}, #{nickname}, #{memberId}, #{title}, #{content}, #{readCount}, #{goodCount}, #{ip}, #{fileAltName})")
+    @Insert("insert into board(boardnum, nickname, memberid, title, content, readCount, goodCount, ip, filealtname) VALUES(#{boardNum}, #{nickname}, #{memberId}, #{title}, #{content}, #{readCount}, #{goodCount}, #{ip}, #{fileAltName})")
     boolean insertContent(BoardVO board);
 
     @Insert("insert into files (filename, filealtname, filepath) VALUES(#{fileName}, #{fileAltName}, #{filePath} )")
     int insertFile(FilesVO files);
 
+    @Insert("insert into goodcount(memberid, boardNum) VALUES(#{memberId}, #{boardNum})")
+    boolean insertGoodCount(String memberId, int boardNum);
+
     @Select("select boardnum, nickname, memberid, title, content, (DATE_FORMAT(writetime, '%Y/%m/%d %h:%i')) as writeTime, readCount, goodCount, filealtname from board where boardnum = #{boardNum}")
     BoardVO selectOneContent(int boardNum);
 
-    @Select("select * from board limit #{firstPage}, #{lastPage}")
+    @Select("select boardnum, title, goodCount, filealtname from board order by boardnum desc limit #{firstPage}, #{lastPage}")
     List<BoardVO> selectContentsList (@Param("firstPage") int firstPage, @Param("lastPage") int lastPage);
     
-    @Select("select * from board where writetime between #{startDay} AND #{endDay} order by goodcount desc LIMIT 0, 19")
+    @Select("select boardnum, title, goodCount, filealtname from board where writetime between #{startDay} and #{endDay} order by goodcount desc limit 0, 19")
     List<BoardVO> selectWeeklyBestList(@Param("startDay") Timestamp statDay, @Param("endDay") Timestamp endDay);
 
     @Select("select title, content, readCount, goodCount, filealtname from board where title LIKE CONCAT('%', #{keyword},'%') limit #{startPage}, #{endPage}")
     List<BoardVO> selectTitleSearch(@Param("keyword") String title, @Param("startPage") int startPage, @Param("endPage") int endPage);
 
+    @Select("select memberid, boardnum from goodcount where memberid = #{memberId} and boardnum = #{boardNum}")
+    Integer checkGoodCount(@Param("memberId") String memberId, @Param("boardNum") int boardNum);
+
     @Update("update board set content = #{content}, title= #{title}, filealtname= #{fileAltName} where boardnum = #{boardNum} ")
     int updateContent(BoardVO board);
-
-    @Update("update files set boardnum = #{boardNum}, where filealtname = #{fileAltName}")
-    void setRealfileNum(int boardNum, String fileAltName);
     
     @Update("update board set goodCount = goodCount +1 where boardnum = #{boardNum} ")
     int updateGoodCount(int boardNum);
