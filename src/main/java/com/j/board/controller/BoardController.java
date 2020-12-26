5d229592290f8af59334196ccd1ad58cab1dd8dd
kit rio <mario64aq@gmail.com>
2020-12-26 10:17:18 +0900

    Add viewcount check cookie

--- src/main/java/com/j/board/controller/BoardController.java
+++ src/main/java/com/j/board/controller/BoardController.java
@@ -1,31 +1,23 @@
 package com.j.board.controller;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.j.board.domain.BoardVO;
 import com.j.board.security.CustomMember;
 import com.j.board.service.BoardListService;
 import com.j.board.service.FileService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @RestController
 @RequestMapping("/list")
@@ -86,26 +78,61 @@ public class BoardController {
     public ResponseEntity<Object> uploadImg(@RequestPart("img") final MultipartFile imgfile) {
 
         String filePath = fileService.upLoadFile(imgfile);
-        if(filePath.equals("invalidfile")){
+        if(filePath.equals("invalidfile") || filePath.equals("fail")){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(filePath, HttpStatus.OK);
     }
 
     @GetMapping("/content/{num}")
-    public ResponseEntity<Object> readContent(@PathVariable("num") int num) {
-
+    public ResponseEntity<Object> readContent(@PathVariable("num") int num, HttpServletRequest request) {
         BoardVO content = boardListService.contentReadService(num);
-        if(content == null ) {
+        if (content == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(content, HttpStatus.OK);
+
+        Cookie[] cookies = request.getCookies();
+        String visitdata = "|";
+        String cookieName = "viewcount";
+        boolean isVisit = false;
+        ResponseCookie responseCookie;
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                if (cookies[i].getName().equals(cookieName)) {
+                    visitdata = cookies[i].getValue();
+                    break;
+                }
+            }
+        }
+
+        if (visitdata.contains(num + "|")) {
+            isVisit = true;
+        } else {
+            boardListService.contentCountUp(num);
+        }
+
+        if(isVisit) {
+            responseCookie = ResponseCookie.from(cookieName, visitdata)
+                    .sameSite("Lax")
+                    .build();
+        } else {
+            responseCookie = ResponseCookie.from(cookieName, num + "|" + visitdata)
+                    .sameSite("Lax")
+                    .build();
+        }
+
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
+                .body(content);
     }
 
     @PutMapping("/content/good/{num}")
-    public ResponseEntity<Object> goodCount(@PathVariable("num") int boardNum) {
+    public ResponseEntity<Object> goodCount(@PathVariable("num") int boardNum, @AuthenticationPrincipal CustomMember principal) {
+        if(principal == null)
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
 
-        if(boardListService.contentGoodCount(boardNum) == 1) {
+        if(boardListService.contentGoodCount(boardNum, principal.getUsername())) {
             return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
